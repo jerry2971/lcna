6e3d7b9176969fae1b868aa3b8c73569fc2a8c6f
jerry <jerry@ecnetworker.com>
2019-07-01 11:26:19 +0800

    ignore build folder

--- templates/app/build.sh
+++ templates/app/build.sh
@@ -1,11 +1,11 @@
 mkdir -p build
 
 cp -r config build
-cp -r ec_modules build
+cp -r lib build
 cp package.json ./build/
 
 node_modules/.bin/babel app.js -o build/app.js
-node_modules/.bin/babel ec_modules -d build/ec_modules
+node_modules/.bin/babel lib -d build/lib
 
 cd build
 npm install --production
