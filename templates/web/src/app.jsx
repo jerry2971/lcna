41480263478fc2bd5d8c980e16506edc9d20052f
jerry <jerry@ecnetworker.com>
2020-05-27 15:28:31 +0800

    lint template

--- templates/web/src/app.jsx
+++ templates/web/src/app.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl';
 import Home from './home.jsx';
 import { httpRequest } from './lib';
 
-const content = document.getElementById("content");
+const content = document.getElementById('content');
 
 const langPath = {
-  'en': '/language/en_US.json',
+  en: '/language/en_US.json',
   'en-US': '/language/en_US.json',
   'zh-TW': '/language/zh_TW.json'
-}
+};
 
-class App extends React.Component{
-
+class App extends React.Component {
   componentDidMount = () => {
     httpRequest('GET', langPath[navigator.language], {}, (statusCode, resJson) => {
       this.setState(resJson);
     });
   }
+
   render() {
     return (
       <div>
@@ -30,4 +30,4 @@ class App extends React.Component{
   }
 }
 
-if(content)ReactDOM.render(<App />, content);
+if (content) ReactDOM.render(<App />, content);
